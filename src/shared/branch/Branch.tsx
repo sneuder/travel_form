471f77a27d364f5d5261528438a1e6bb9416da7d
Esneider Ospina <sneuder8@gmail.com>
2022-05-21 19:53:12 -0500

    branch

---
+++ src/shared/branch/Branch.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import style from './branch.module.scss';
+
+const Branch = () => {
+  return <h1 className={style.branchTitle}>Traveling</h1>;
+}
+
+export default Branch;
